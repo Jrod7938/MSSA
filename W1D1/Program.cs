ab8e4cf57a37122cf6ac07965bbd6118e2ce50af
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-01-13 13:00:34 -0800

    W1D1
    initial commit of week 1 day 1

---
+++ W1D1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1D1 { // Scope
+    internal class Program { // Starting Point
+        static void Main(string[] args) { 
+            Console.WriteLine("Hello World!");
+
+            var response = Console.ReadLine();
+            Console.WriteLine($"Hello, {response}.");
+        }
+    }
+}
