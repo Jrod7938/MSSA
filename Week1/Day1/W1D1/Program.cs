ae26d0312975d951e05c0bea21d0cebc7be83db3
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-13 14:58:46 -0800

    Uploaded files for week 1 day 1

---
+++ Week1/Day1/W1D1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1D1 { // Scope
+    internal class Program { // Starting Point
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What is your name?");
+            string response = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {response}.");
+        }
+    }
+}
---
+++ Week1/Day1/W1D1Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1D1Assignment {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Solution.Part1();
+            Solution.Part2();
+            Solution.Part3();
+        }
+    }
+}
