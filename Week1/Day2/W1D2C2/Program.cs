b62743072389d9fdd46753ced01e36c9622cadb2
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-14 11:02:10 -0800

    Week 1 Day 2 Code 2

---
+++ Week1/Day2/W1D2C2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1D2C2 {
+    internal class Program {
+        // If Else Demo
+        static void Main(string[] args) {
+            Console.WriteLine("Speed Checker");
+            Console.WriteLine("Enter speed: ");
+            int speed = int.Parse(Console.ReadLine());
+
+            CheckSpeed(speed);
+
+        }
+
+        static void CheckSpeed(int speed) {
+            if (speed > 55) {
+                if (speed > 55 && speed <= 65) {
+                    Console.WriteLine("Within limits");
+                } else if (speed > 85) {
+                    Console.WriteLine("Reckless driving, license revoked!");
+                } else {
+                    Console.WriteLine("You are speeding");
+                }
+            } else {
+                Console.WriteLine("Within limits");
+            }
+        }
+    }
+}
