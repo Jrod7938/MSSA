64b7e5437cf514443b934e06c2e47645a902b45d
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-21 13:18:50 -0800

    Week 2 Day 1 Assignment Part 1

---
+++ Week2/Day1/W2D1Assignment/Part1/Ammunition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2D1Assignment.Part1 {
+    abstract class Ammunition {
+        private double velocity;
+        public double Velocity {  get { return velocity; } set { velocity = value; } }
+        private double weight;
+        public double Weight {  get { return weight;  } set { if (value >= 0) weight = value; } }
+    }
+}
---
+++ Week2/Day1/W2D1Assignment/Part1/Pistol/9mm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2D1Assignment.Part1 {
+    internal class _9mm : PistolAmmo {
+       
+    }
+}
