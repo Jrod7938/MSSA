bdf152d1376e16a5f021300e577901a1fc551fc2
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-22 10:58:19 -0800

    Week 2 Day 2 Code 3 initial

---
+++ Week2/Day2/W2D2C3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2D2C3 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            DriveInfo drive = new DriveInfo(@"D:\"); 
+        }
+    }
+}
