5fa96a25d437db2c91c77b62c13bf9982357d424
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-22 11:17:58 -0800

    Week 2 Day 2 Code 3

--- Week2/Day2/W2D2C3/Program.cs
+++ Week2/Day2/W2D2C3/Program.cs
@@ -9,7 +9,20 @@ namespace W2D2C3 {
     internal class Program {
         static void Main(string[] args) {
 
-            DriveInfo drive = new DriveInfo(@"D:\"); 
+            DriveInfo drive = new DriveInfo(@"D:\");
+            Console.WriteLine($"Drive Name {drive.Name}\nDrive Size: {drive.TotalSize}\nDrive Free Space: {drive.TotalFreeSpace}");
+            DirectoryInfo info = new DirectoryInfo(@"D:\Nerd");
+            DirectoryInfo[] directories = info.GetDirectories();
+            Console.WriteLine("Directories: ");
+            foreach (DirectoryInfo directory in directories) { 
+                Console.WriteLine(directory.Name);
+            }
+
+            Console.WriteLine("Files: ");
+            FileInfo[] fileInfos = info.GetFiles();
+            foreach(FileInfo fileInfo in fileInfos) {
+                Console.WriteLine($"File Name: {fileInfo.Name}\nFile Created: {fileInfo.CreationTime}");
+            }
         }
     }
 }
