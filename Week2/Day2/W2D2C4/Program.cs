bfb3537bed170e62bb9f45d0620f874e99102c38
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-22 13:16:07 -0800

    Week 4 Day 2 Code 4

---
+++ Week2/Day2/W2D2C4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2D2C4 {
+    internal class Program {
+        static void Main(string[] args) {
+            const double tipRate = 0.18;
+
+            if(args.Length == 0 || !double.TryParse(args[0], out double billTotal)) {
+                Console.WriteLine("Please enter the bill amount.");
+                return;
+            }
+
+            double tip = billTotal * tipRate;
+            Console.WriteLine();
+            Console.WriteLine("".PadRight(25, '*'));
+            Console.WriteLine($"Bill Total: \t{billTotal,8:c}");
+            Console.WriteLine($"Tip: \t\t{tip,8:c}");
+            Console.WriteLine($"Tip Rate: \t{tipRate,8:p}");
+            
+            Console.WriteLine("".PadRight(25, '-'));
+            Console.WriteLine($"Total: \t\t{billTotal + tip,8:c}");
+            Console.WriteLine("".PadRight(25, '*'));
+        }
+    }
+}
