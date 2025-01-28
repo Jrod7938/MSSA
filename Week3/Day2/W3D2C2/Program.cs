bbcc1ba73e0b9425b124d4dbd5884c6d6a4deccc
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-28 10:27:48 -0800

    Week 3 Day 2 Code 2

---
+++ Week3/Day2/W3D2C2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3D2C2 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            int[][] tempCities = new int[4][]; // decide the number of rows (cities)
+
+            for(int i = 0; i < tempCities.Length; i++) {
+                Console.WriteLine($"Enter the number of readings you expect for city {i}");
+                int numCities = int.Parse(Console.ReadLine());
+
+                tempCities[i] = new int[numCities]; // allocate size by creeating array and adding it to cities
+                for(int j = 0; j < tempCities[i].Length; j++) {
+                    Console.WriteLine($"Enter the temperature for [{i},{j}]");
+                    tempCities[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Jagged array");
+            for (int i = 0; i < tempCities.Length; i++) {
+                for(int j = 0; j < tempCities[i].Length; j++) {
+                    Console.Write(tempCities[i][j] + "F\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
