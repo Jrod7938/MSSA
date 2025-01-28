ad55c948840d990f0ff6e8a52218e68b54c32f17
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-28 15:00:01 -0800

    Week 3 Day 2 Assignment 3.2

---
+++ Week3/Day2/W3D2Assignment3.2/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3D2Assignment3._2 {
+    internal class Circle {
+        public double Radius { get; set; }
+
+        public Circle(double radius) {
+            Radius = radius;
+        }
+
+        public double GetArea() {
+            return Math.PI * Radius * Radius;
+        }
+
+        public static double operator +(Circle circle1, Circle circle2) {
+            return circle1.GetArea() + circle2.GetArea();
+        }
+
+        public static double operator -(Circle circle1, Circle circle2) {
+            return Math.Abs(circle1.GetArea() - circle2.GetArea());
+        }
+    }
+}
