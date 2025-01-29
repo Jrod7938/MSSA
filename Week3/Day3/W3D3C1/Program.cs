188882c4b0f9272d6375d3a6a933a7b206cca285
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-29 09:25:49 -0800

    Week 3 Day 3 Code 1

---
+++ Week3/Day3/W3D3C1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace W3D3C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PrintFavSeason(Seasons.Summer);
+        }
+
+        static void PrintFavSeason(Seasons season) {
+            int val = (int)season;
+            Console.WriteLine($"Your favourite season is {season} - {val}.");
+            Console.WriteLine($"Error Code example: {ErrorCodes.Internal_Server_Error}");
+        }
+    }
+}
