84f29951299b87b997ba17567554cf7d9d2d8961
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-30 10:38:27 -0800

    Week 3 Day 4 Code 2

---
+++ Week3/Day4/W3D4C2/Solution.cs
@@ -0,0 +1,19 @@
+﻿
+namespace W3D4C2 {
+    internal class Solution {
+
+        internal static int BuyChocolate(ref int[] ints, int money) {
+            Array.Sort(ints);
+
+            if (ints.Length < 2) return money;
+
+            int minValueCost = ints[0] + ints[1];
+
+            if(minValueCost <= money) {
+                return money - minValueCost;
+            }
+
+            return money;
+        }
+    }
+}
