bd6f7be4fb1b6782a0d18574c403c9e77c097301
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-30 09:42:03 -0800

    Week 3 Day 4 Code 1

---
+++ Week3/Day4/W3D4C1/FoodMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3D4C1 {
+    internal class FoodMenu {
+        private List<string> foodItems;
+
+        public FoodMenu(List<string> foodItems) {
+            this.foodItems = foodItems;
+        }
+
+        public int Count { get { return foodItems.Count; } }
+
+        public string this[int index] {
+            get { 
+                if(index < this.Count && index > -1) {
+                    return this.foodItems[index];
+                }
+                return string.Empty;
+            }
+            set {
+                if (!this.foodItems.Contains(value)) {
+                    this.foodItems.Add(value);
+                }
+            }
+        }
+
+    }
+}
---
+++ Week3/Day4/W3D4C1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace W3D4C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodMenu weekendMenu = new FoodMenu(new List<string> { "Pizza", "Burgers", "Pasta", "Salad" });
+            for(int i = 0; i < weekendMenu.Count; i++) {
+                Console.WriteLine(weekendMenu[i]);
+            }
+
+            weekendMenu[weekendMenu.Count] = GetInput<string>("Enter a new food item: ");
+
+            for (int i = 0; i < weekendMenu.Count; i++) {
+                Console.WriteLine(weekendMenu[i]);
+            }
+        }
+
+        private static T GetInput<T>(T v) {
+            Console.Write(v);
+            string ?input = Console.ReadLine();
+            try {
+                var userInput = Convert.ChangeType(input, typeof(T));
+                return (T)userInput;
+            } catch (Exception exception){
+                Console.WriteLine($"Please enter a type of {typeof(T).Name}.\nError Message: {exception.Message}");
+                return GetInput<T>(v);
+            }
+        }
+    }
+}
