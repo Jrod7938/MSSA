e0e7fe5bba6d3cdc45499dc4cdbc13a887a5f9f5
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-01-30 16:38:46 -0800

    Week 3 Challenge Labs

---
+++ Week3/Week3ChallengeLab/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Week3ChallengeLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Part1();
+            Solution.Part2();
+            Solution.Part3();
+            Solution.Part4();
+        }
+    }
+}
