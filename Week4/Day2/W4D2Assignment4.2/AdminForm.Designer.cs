4b1d91b38a0a32027d2d9ecf285d44d20d12dbb5
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-04 15:21:20 -0800

    Week 4 Day 2 Assignment 4.2

---
+++ Week4/Day2/W4D2Assignment4.2/AdminForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace W4D2Assignment4._2 {
+    partial class AdminForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            dgStudents = new DataGridView();
+            txtName = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            nudGPA = new NumericUpDown();
+            btnDelete = new Button();
+            btnSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgStudents).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudGPA).BeginInit();
+            SuspendLayout();
+            // 
+            // dgStudents
+            // 
+            dgStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgStudents.Location = new Point(12, 12);
+            dgStudents.Name = "dgStudents";
+            dgStudents.Size = new Size(560, 426);
+            dgStudents.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(73, 22);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(131, 23);
+            txtName.TabIndex = 1;
+            txtName.Validating += txtName_Validating;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 3;
+            label2.Text = "GPA";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(nudGPA);
+            groupBox1.Controls.Add(txtName);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(578, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(210, 147);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "New Student";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(73, 108);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // nudGPA
+            // 
+            nudGPA.DecimalPlaces = 2;
+            nudGPA.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
+            nudGPA.Location = new Point(73, 58);
+            nudGPA.Maximum = new decimal(new int[] { 40, 0, 0, 65536 });
+            nudGPA.Name = "nudGPA";
+            nudGPA.Size = new Size(131, 23);
+            nudGPA.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(603, 233);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(707, 233);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSave);
+            Controls.Add(btnDelete);
+            Controls.Add(groupBox1);
+            Controls.Add(dgStudents);
+            Name = "AdminForm";
+            Text = "Admin Form";
+            ((System.ComponentModel.ISupportInitialize)dgStudents).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudGPA).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgStudents;
+        private TextBox txtName;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox1;
+        private NumericUpDown nudGPA;
+        private Button button1;
+        private Button btnDelete;
+        private Button btnSave;
+    }
+}
---
+++ Week4/Day2/W4D2Assignment4.2/LoginForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace W4D2Assignment4._2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnLogin = new Button();
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label1.Location = new Point(68, 121);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 17);
+            label1.TabIndex = 0;
+            label1.Text = "UserName:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label2.Location = new Point(68, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(149, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 37);
+            label3.TabIndex = 2;
+            label3.Text = "LOGIN";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            btnLogin.Location = new Point(127, 200);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(131, 40);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtUserName
+            // 
+            txtUserName.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            txtUserName.Location = new Point(149, 113);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(156, 25);
+            txtUserName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            txtPassword.Location = new Point(149, 147);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(156, 25);
+            txtPassword.TabIndex = 5;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(382, 283);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Controls.Add(btnLogin);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "Login";
+            Enter += btnLogin_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnLogin;
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+    }
+}
