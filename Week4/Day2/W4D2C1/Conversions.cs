87cba8098e8082e770ac8f107c750dc2f15eb071
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-04 09:51:27 -0800

    Week 4 Day 2 Code 1

---
+++ Week4/Day2/W4D2C1/Conversions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4D2C1 {
+    static class Conversions { 
+        public static double CelciusToFahrenheit(double celcius) {
+            return celcius * (9/5) + 32;
+        }
+    }
+}
+ 
---
+++ Week4/Day2/W4D2C1/Swap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4D2C1 {
+
+    // T : Placeholder for dataType
+    internal class Swap<T> {
+        public static void SwapData(ref T num1, ref T num2) {
+            T temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+    }
+}
