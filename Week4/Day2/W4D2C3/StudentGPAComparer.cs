3051448b51f74d773181b456069aa3b4b7cba6e5
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-04 11:27:21 -0800

    Week 4 Day 2 Code 3

---
+++ Week4/Day2/W4D2C3/StudentGPAComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4D2C3 {
+    internal class StudentGPAComparer : IComparer<Student> {
+        public int Compare(Student? x, Student? y) {
+            return x.Gpa.CompareTo(y.Gpa);
+        }
+    }
+}
