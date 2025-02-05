9ad3e9fe15da55bb7bab6fecc77f45a4593c869e
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-05 15:49:07 -0800

    LeetCode 344. Reverse String

---
+++ LeetCode/344/ReverseString344/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ReverseString344
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = { 'H', 'a', 'n', 'n', 'a', 'h' };
+            ReverseString(s);
+            Console.WriteLine(s);
+        }
+
+        private static void ReverseString(char[] s) {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i <= j) {
+                (s[i], s[j]) = (s[j], s[i]);
+                i++;
+                j--;
+            }
+        }
+    }
+}
