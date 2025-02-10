3986e9d0d9913b53ee3d8ee61520f195b31505f3
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-10 08:44:37 -0800

    Week 5 Day 1 Code 1

---
+++ Week5/Day1/W5D1C1/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5D1C1 {
+    // Immutable Object
+    internal class Document {
+        private string name;
+        private string content;
+        public Document(string name, string content) {
+            this.name = name;
+            this.content = content;
+        }
+        // readonly - immutable
+        public string Name { get { return name; } }
+        public string Content {  get { return content; } }
+    }
+}
