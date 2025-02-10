891775e9e94d2be3bf0245b89ff10b5c8f08b8a6
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-10 13:51:03 -0800

    LeetCode 9. Palindrome Number

---
+++ LeetCode/9/PalindromeNumber9/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace PalindromeNumber9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(121));
+            Console.WriteLine(IsPalindrome(-121));
+            Console.WriteLine(IsPalindrome(10));
+        }
+
+        private static bool IsPalindrome(int x) {
+            if (x < 0) return false;
+            int original = x;
+            int reversed = 0;
+            
+            while(x > 0) {
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+
+            return original == reversed;
+        }
+    }
+}
