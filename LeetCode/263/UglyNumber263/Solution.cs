43a72318ca8626ea5e67c5c8b75ff5861a4eb677
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-10 13:43:09 -0800

    LeetCode 263. Ugly Number (#25)

---
+++ LeetCode/263/UglyNumber263/Solution.cs
@@ -0,0 +1,16 @@
+﻿
+namespace UglyNumber263 {
+    internal class Solution {
+        internal static bool IsUgly(int n) {
+            if (n <= 0) return false;
+            int[] primes = { 2, 3, 5 };
+
+            foreach(int prime in primes) {
+                while(n % prime == 0) { // n is divisible by prime
+                    n /= prime; // divide n by prime
+                }
+            }
+            return n == 1;
+        }
+    }
+}
