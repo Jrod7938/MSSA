7a32791b234179fef6e02ff7585f79c7eae30fc6
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-10 13:42:45 -0800

    LeetCode 263. Ugly Number

---
+++ LeetCode/263/UglyNumber263/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UglyNumber263
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.IsUgly(6));
+            Console.WriteLine(Solution.IsUgly(1));
+            Console.WriteLine(Solution.IsUgly(14));
+
+        }
+    }
+}
