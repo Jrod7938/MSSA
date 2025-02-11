3534e4eda412a24e12b84986343bed6d09341f77
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-11 13:05:05 -0800

    Week 5 Day 2 (#28)
    * Week 5 Day 2 Code 1
    
    * Week 5 Day 2 Code 2
    
    * Week 5 Day 2 Code 3
    
    * Week 5 Day 2 Assignment 5.2

---
+++ Week5/Day2/W5D2C1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace W5D2C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = @"C:\Users\guana";
+            //string[] directories = Directory.GetDirectories(PATH);
+
+            //foreach(string directory in directories) {
+            //    Console.WriteLine(directory);
+            //    string[] subDirectory = Directory.GetDirectories(directory);
+            //    foreach(string subDir in subDirectory) {
+            //        Console.WriteLine(subDir);
+            //        // too much work!! Can be done with recursion
+            //    }
+            //}
+
+            PrintAllDirectories(PATH, 8);
+        }
+
+        private static void PrintAllDirectories(string path, int depth) {
+            if (depth <= 0) return;
+            try {
+                string[] directories = Directory.GetDirectories(path);
+                foreach (string directory in directories) {
+                    Console.WriteLine(directory);
+                    PrintAllDirectories(directory, depth - 1);
+                }
+            } catch (Exception exeption) {
+                Console.WriteLine($"Access denied to path: {exeption.Message}");
+            }
+        }
+    }
+}
---
+++ Week5/Day2/W5D2C3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace W5D2C3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum by Iteration: " + SumOfNumbersIterative(25));
+            Console.WriteLine("Sum by Recursion: " + SumOfNumbersRecursive(25));
+        }
+
+        /// <summary>
+        /// This method calculates the sum of numbers from 1 to n using iteration.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int SumOfNumbersIterative(int n) {
+            int sum = 0;
+            int i = 1;
+            while(i <= n) {
+                sum += i;
+                i++;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// This method calculates the sum of numbers from 1 to n using recursion.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int SumOfNumbersRecursive(int n) {
+            if (n == 1) return n;
+            return n + SumOfNumbersRecursive(n - 1);
+        }
+    }
+}
