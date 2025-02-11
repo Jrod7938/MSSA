8b9ff9be36c3d642c194866650403f9a8ecf9bc6
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-11 10:01:57 -0800

    Week 5 Day 2 Code 2

---
+++ Week5/Day2/W5D2C2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace W5D2C2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iterative: ");
+            PrintSquaresIteration(6);
+            Console.WriteLine("\nTail Recursive: ");
+            PrintSquaresTailRecursive(6);
+            Console.WriteLine("\nHead Recursive: ");
+            PrintSquaresHeadRecursive(6);
+        }
+
+        static void PrintSquaresIteration(int n) {
+            while(n > 0) {
+                Console.Write(n * n + " ");
+                n--;
+            }
+        }
+
+        static void PrintSquaresTailRecursive(int n) {
+            if (n < 1) return;
+            Console.Write(n * n + " ");
+            PrintSquaresTailRecursive(n - 1);
+        }
+        static void PrintSquaresHeadRecursive(int n) {
+            if (n < 1) return;
+            PrintSquaresHeadRecursive(n - 1);
+            Console.Write(n * n + " ");
+        }
+    }
+}
