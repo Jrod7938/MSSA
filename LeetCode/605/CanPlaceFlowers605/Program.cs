90457df5ccda7787d99a4e38e5f97e84e70e5aa6
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-12 13:36:04 -0800

    LeetCode 605. Can Place Flowers (#30)

---
+++ LeetCode/605/CanPlaceFlowers605/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CanPlaceFlowers605
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanPlaceFlowers(new int[] {1, 0, 0, 0, 0, 0, 1}, 2));
+        }
+
+        private static bool CanPlaceFlowers(int[] flowerbed, int n) {
+            for(int i = 0; i < flowerbed.Length; i++) {
+                if (flowerbed[i] == 0 && 
+                    (i == 0 || flowerbed[i - 1] == 0) && // check left
+                    (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) { // check right
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+            return n <= 0;
+        }
+    }
+}
