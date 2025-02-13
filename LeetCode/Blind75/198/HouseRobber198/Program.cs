5e01cb30cf8bfa5f8b71c3bb9bd71e7e15018444
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-13 14:19:27 -0800

    LeetCode 198. House Robber (#35)

---
+++ LeetCode/Blind75/198/HouseRobber198/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HouseRobber198
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rob(new int[] { 2, 7, 9, 3, 1 }));
+        }
+        
+        private static int Rob(int[] nums) {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+
+            int maxMoneyPrevHouse = 0, // max money robbed previous house
+                maxMoneyTwoHousesAgo = 0; // max money robbed 2 houses ago
+
+            foreach (int currentHouseMoney in nums) {
+                int tempMax = maxMoneyPrevHouse; // store previous max
+                maxMoneyPrevHouse = Math.Max(maxMoneyTwoHousesAgo + currentHouseMoney, maxMoneyPrevHouse); // get max between houses not adjacent 
+                maxMoneyTwoHousesAgo = tempMax;
+            }
+
+            return maxMoneyPrevHouse;
+        }
+    }
+}
