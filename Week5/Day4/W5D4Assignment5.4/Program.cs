2cf5023328edf0bf0e1bb3500c3f323812149f8a
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-13 13:35:41 -0800

    Week 5 Day 4 Assignment 5.4

---
+++ Week5/Day4/W5D4Assignment5.4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W5D4Assignment5._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Part1();
+            Solution.Part2();
+        }
+    }
+}
