67f81cfa3ba9371b35b8c8b5af44d89d132f1656
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-18 13:32:56 -0800

    Week 6 Day 1 Code 2

---
+++ Week6/Day1/W6D1C2/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6D1C2 {
+    internal class Node<T> {
+        public T data; // data
+        public Node<T> next; // address of next node
+
+        public Node(T data, Node<T> next){
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
---
+++ Week6/Day1/W6D1C2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace W6D1C2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Custom Linked List");
+
+            LinkedListClass linkedList = new LinkedListClass();
+
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+
+            linkedList.Display();
+
+            linkedList.AddLast(3);
+            linkedList.Display();
+            linkedList.RemoveFirst();
+            linkedList.Display();
+            linkedList.AddLast(10);
+            linkedList.Display();
+            linkedList.RemoveLast();
+            linkedList.Display();
+
+            int found;
+            Console.WriteLine($"{linkedList.Search(3, out found)} index={found}");
+
+            linkedList.AddAnywhere(2, 2);
+            linkedList.Display();
+
+            linkedList.RemoveAnywhere(3);
+            linkedList.Display();
+        }
+    }
+}
