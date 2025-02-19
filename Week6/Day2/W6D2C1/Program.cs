11a63047a6c6e5d964a92495e4e90ddf2df5cc8e
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-19 09:23:56 -0800

    Week 6 Day 2 Code 1

---
+++ Week6/Day2/W6D2C1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace W6D2C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            Stack<string> operations = new Stack<string>();
+            StackArray<int> myStack = new StackArray<int>(10);
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.Push(5);
+            myStack.Display();
+            myStack.Pop();
+            myStack.Display();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Display();
+
+        }
+    }
+}
