f54f8f562c99c51e548aef753f57e4f0dc233353
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-18 16:03:29 -0800

    LeetCode 283. Move Zeroes

---
+++ LeetCode/Blind75/283/MoveZeros283/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MoveZeros283
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            MoveZeroes(nums);
+            foreach (var num in nums) {
+                Console.Write(num + " ");
+            }
+        }
+
+        private static void MoveZeroes(int[] nums) {
+            int pointer = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (nums[i] != 0) {
+                    (nums[i], nums[pointer]) = (nums[pointer], nums[i]);
+                    pointer++;
+                }
+            }
+        }
+    }
+}
