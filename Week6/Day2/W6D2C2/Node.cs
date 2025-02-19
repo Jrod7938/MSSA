9434759f797b38d3281901f7c14b7bd3c4a4dd08
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-19 10:26:37 -0800

    Week 6 Day 2 Code 2

---
+++ Week6/Day2/W6D2C2/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6D2C2 {
+    internal class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T val, Node<T> next = null){
+            this.data = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Week6/Day2/W6D2C2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace W6D2C2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackLL<int> stack = new StackLL<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Display();
+            Console.WriteLine("After Pop");
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            stack.Display();
+        }
+    }
+}
