2034c257228e002fcacd0d65e78a80d2d51a0547
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-19 13:16:30 -0800

    Week 6 Day 2 Code 3

---
+++ Week6/Day2/W6D2C3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace W6D2C3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 23, 1, 45, 2, 12, 99 };
+            BubbleSort(array);
+            foreach (int number in array) Console.WriteLine(number);
+        }
+
+        public static void BubbleSort(int[] array) {
+            for(int pass = array.Length - 1; pass >= 0; pass--) {
+                for(int i=0; i < pass; i++) {
+                    if (array[i] > array[i + 1]) {
+                        (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                    }
+                }
+            }
+        }
+    }
+}
