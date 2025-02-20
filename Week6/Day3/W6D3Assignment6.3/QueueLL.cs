e93a88c17ea76902bc6cd16d4f5ce5bbb413c2f0
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-20 13:19:21 -0800

    Week 6 Day 3 Assignment 6.3

---
+++ Week6/Day3/W6D3Assignment6.3/QueueLL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6D3Assignment6._3 {
+    internal class QueueLL<T> {
+        Node<T> front;
+        Node<T> rear;
+        int size;
+
+        public QueueLL() {
+            this.front = null;
+            this.rear = null;
+            this.size = 0;
+        }
+
+        public bool IsEmpty() { return this.size == 0; }
+
+        public void Enqueue(T val) {
+            Node<T> newNode = new Node<T>(val);
+            if (IsEmpty()) {
+                front = newNode;
+            } else {
+                rear.next = newNode; 
+            }
+            rear = newNode; 
+            size++;
+        }
+
+        public T Peek() { return front.data; }
+
+        public T Dequeue() {
+            if (IsEmpty()) { throw new InvalidOperationException("Queue is empty"); }
+            Node<T> temp = front;
+            front = front.next;
+            size--;
+            return temp.data;
+        }
+
+        public void Display() {
+            if (IsEmpty()) { Console.WriteLine("Queue is empty"); return; }
+            Node<T> current = front;
+            while (current != null) {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
