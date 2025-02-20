f270b8c9c8f9ede198b4af8b054f55543665abe1
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-20 14:01:17 -0800

    LeetCode 48. Rotate Image (#42)

---
+++ LeetCode/LeetCode150/48/RotateImage48/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RotateImage48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix = new int[][] {
+                new int[] {1, 2, 3},
+                new int[] {4, 5, 6},
+                new int[] {7, 8, 9}
+            };
+            Rotate(matrix);
+        }
+
+        public static void Rotate(int[][] matrix) {
+            for (int i = 0; i < matrix.Length; i++) { // transpose
+                for (int j = i + 1; j < matrix.Length; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+
+            for (int i = 0; i < matrix.Length; i++) { // reverse
+                Array.Reverse(matrix[i]);
+            }
+        }
+    }
+}
