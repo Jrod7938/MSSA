d3747a4b25f11027c5c78c0ff74e14dff44c6bff
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-24 10:15:44 -0800

    Week 7 Day 1 Code 2

---
+++ Week7/Day1/W7D1C2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace W7D1C2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSort(new int[] { 3, 5, 9, 8, 6, 2});
+        }
+
+        static void InsertionSort(int[] array) {
+            int temp = 0;
+            int position = 0;
+
+            for(int i = 0; i < array.Length; i++) {
+                temp = array[i];
+                position = i;
+                while(position > 0 && array[position - 1] > temp) {
+                    array[position] = array[position - 1];
+                    position--;
+                }
+                array[position] = temp;
+            }
+        }
+    }
+}
