cd3e2a6784563847b8d1c555440403423ee31e6e
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-24 09:11:38 -0800

    Week 7 Day 1 Code 1

---
+++ Week7/Day1/W7D1C1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace W7D1C1 {
+    internal class Program {
+        static void Main(string[] args) {
+            SelectionSort(new int[] { 5, 3, 9, 8, 6, 2 });
+        }
+
+        static void SelectionSort(int[] array) {
+            int minIndex = 0; // track min index
+            int temp = 0;
+            for (int i = 0; i < array.Length - 1; i++) {
+                minIndex = i;
+                for (int j = i + 1; j < array.Length; j++) { // get min element index 
+                    if (array[j] < array[minIndex]) {
+                        minIndex = j; // update min index
+                    }
+                }
+                if (minIndex != i) { // swap
+                    temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+        }
+    }
+}
