95f1b0c1793b0999fe64c569a1f71f77ccb8360c
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-24 11:00:58 -0800

    Week  Day 1 Assignment 7.1

---
+++ Week7/Day1/W7D1Assignment7.1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W7D1Assignment7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Part1();
+            Solution.Part2();
+        }
+    }
+}
