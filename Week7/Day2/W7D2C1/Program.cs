e5420398bbf0b90300928740e219c8f8a9c94e59
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-25 09:43:30 -0800

    Week 7 Day 2 Code 1

---
+++ Week7/Day2/W7D2C1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace W7D2C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShellSort(new int[] { 3, 5, 8, 9, 6, 2 });
+        }
+
+        // O(nlog(n))
+        private static void ShellSort(int[] A) {
+            int gap = A.Length / 2;
+            int i,
+                j;
+
+            while (gap > 0) {
+                i = gap;
+                while (i < A.Length) {
+                    int temp = A[i]; // number to be placed
+                    j = i - gap;
+                    while(j >= 0 && A[j] > temp) {
+                        A[j+gap] = A[j];
+                        j = j - gap;
+                    }
+                    A[j + gap] = temp; // final position for temp
+                    i++;
+                }
+            }
+        }
+    }
+}
