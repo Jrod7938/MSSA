0dc421da78ba9585bfdb0a39d83932d992432828
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-26 10:37:07 -0800

    Week 7 Day 3 Code 1

---
+++ Week7/Day3/W7D3C1/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace W7D3C1 {
+    class Node {
+        public int data;
+        public Node right;
+        public Node left;
+
+        public Node(int data, Node right = null, Node left = null) {
+            this.data = data;
+            this.right = right;
+            this.left = left;
+        }
+    }
+}
---
+++ Week7/Day3/W7D3C1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace W7D3C1 {
+    internal class Program {
+        static void Main(string[] args) {
+            BinarySearchTree tree = new BinarySearchTree();
+
+            tree.InsertNode(tree.Root, 100);
+            tree.InsertNode(tree.Root, 200);
+            tree.InsertNode(tree.Root, 20);
+            tree.InsertNode(tree.Root, 300);
+            tree.InsertNode(tree.Root, 150);
+            tree.InsertNode(tree.Root, 10);
+            tree.InsertNode(tree.Root, 30);
+
+            Console.WriteLine("PreOrder");
+            tree.PreOrder(tree.Root);
+            Console.WriteLine("\nInOrder");
+            tree.InOrder(tree.Root);
+            Console.WriteLine("\nPostOrder");
+            tree.PostOrder(tree.Root);
+
+            
+
+            Console.WriteLine($"\nFound 20: {tree.Search(20)}");
+        }
+    }
+}
