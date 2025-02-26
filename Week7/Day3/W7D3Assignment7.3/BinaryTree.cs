1189abd918c38bd49d9e0ca26136422c9b09b89e
Jancarlos Rodriguez <jrod7938@gmail.com>
2025-02-26 12:22:49 -0800

    Week 7 Day 3 Assignment 7.3

---
+++ Week7/Day3/W7D3Assignment7.3/BinaryTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W7D3Assignment7._3 {
+    internal class BinaryTree {
+        public Node Root { get; private set; }
+
+        public BinaryTree(Node root = null) {
+            this.Root = root;
+        }
+
+        public void PrintTree(Node root, int space = 0, int indent = 4) {
+            if (root == null) return;
+
+            PrintTree(root.right, space + indent, indent);
+            Console.WriteLine(new string(' ', space) + root.val);
+            PrintTree(root.left, space + indent, indent);
+        }
+
+        internal void InsertNode(Node node) {
+            if (Root == null) {
+                Root = node;
+                return;
+            }
+
+            Node current = Root;
+            Node parent = null;
+
+            while (current != null) {
+                parent = current;
+                if (node.val == current.val) {
+                    return; 
+                } else if (node.val < current.val) {
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+            }
+
+            if (node.val < parent.val) {
+                parent.left = node;
+            } else {
+                parent.right = node;
+            }
+        }
+
+
+        internal BinaryTree Search(int number) {
+            Node temp = Root;
+            while(temp != null) {
+                if (temp.val == number) return new BinaryTree(temp);
+                else if(temp.val > number) {
+                    temp = temp.left;
+                } else {
+                    temp = temp.right;
+                }
+            }
+            return new BinaryTree();
+        }
+    }
+}
