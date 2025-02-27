1120bddb962f56767da361825f47371da339a403
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-02-26 18:40:07 -0800

    MidTerm: Try Move Update (#60)
    closes #46

--- Midterm/Checkers/Assets/Scripts/Core/Piece.cs
+++ Midterm/Checkers/Assets/Scripts/Core/Piece.cs
@@ -7,6 +7,50 @@ public class Piece : MonoBehaviour
     public bool isRed;
     public bool isKing;
 
+    public bool IsForcedToMove(Piece[,] board, int x, int y){
+        if(isRed || isKing){  
+            if(x >= 2 && y <= 5){ // top left
+                Piece piece = board[x - 1, y + 1];
+                if(piece != null && piece.isRed != isRed){
+                    if(board[x - 2, y + 2] == null){
+                        return true;
+                    }
+                }
+            }
+
+            if(x <= 5 && y <= 5){ // top right
+                Piece piece = board[x + 1, y + 1];
+                if(piece != null && piece.isRed != isRed){
+                    if(board[x + 2, y + 2] == null){
+                        return true;
+                    }
+                }
+            }
+        } 
+        
+        if(!isRed || isKing) {
+            if(x >= 2 && y >= 2){ // bottom left
+                Piece piece = board[x - 1, y - 1];
+                if(piece != null && piece.isRed != isRed){
+                    if(board[x - 2, y - 2] == null){
+                        return true;
+                    }
+                }
+            }
+
+            if(x <= 5 && y >= 2){ // bottom right
+                Piece piece = board[x + 1, y - 1];
+                if(piece != null && piece.isRed != isRed){
+                    if(board[x + 2, y - 2] == null){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2){
         // moving on top of another piece
         if(board[x2,y2] != null) return false;
