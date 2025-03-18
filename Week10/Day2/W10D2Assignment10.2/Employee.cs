5b2ad7c4848c947017ad855e1abe60009e336661
Jrod7938 <Jrod7938@gmail.com>
2025-03-18 16:53:16 -0700

    Week 10 Day 2 Assignment 10.2

---
+++ Week10/Day2/W10D2Assignment10.2/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace W10D2Assignment10._2 {
+    class Employee {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+    }
+
+}
