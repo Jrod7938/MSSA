38e855a93e69d470a8eabf7bede99fc97781238d
Jrod7938 <Jrod7938@gmail.com>
2025-03-19 21:56:50 -0700

    Week 10 Day 3 Assignment 10.3

---
+++ Week10/Day3/W10D3Assignment10.3/Data/Records.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W10D3Assignment10._3.Data {
+    internal class Records {
+        public static CarContext carContext;
+    }
+}
