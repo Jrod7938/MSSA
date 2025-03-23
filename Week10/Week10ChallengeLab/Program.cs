410d7ccae079f33087da5b25f3e88b5ab69f766f
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-03-23 16:10:10 -0700

    Week 10 Challenge Labs (#77)

---
+++ Week10/Week10ChallengeLab/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Week10ChallengeLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Part1();
+            Solution.Part2();
+        }
+    }
+}
