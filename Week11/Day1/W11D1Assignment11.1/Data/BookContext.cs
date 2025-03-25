54e1ae51afef647d4e10e5c52c7d4d85a81de2b0
Jrod7938 <Jrod7938@gmail.com>
2025-03-24 20:40:09 -0700

    Week 11 Day 1 Assignment 11.1

---
+++ Week11/Day1/W11D1Assignment11.1/Data/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W11D1Assignment11._1.Data {
+    public class BookContext : DbContext {
+        public DbSet<Book> Books { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Book>().HasKey(b => b.ISBN);
+            modelBuilder.Entity<Book>().HasData(
+                new Book { ISBN = "9780134685991", Name = "Effective Java", Author = "Joshua Bloch", Description = "Java programming best practices" },
+                new Book { ISBN = "9781492078005", Name = "Learning C#", Author = "Jesse Liberty", Description = "Beginner C# guide" }
+                );
+        }
+
+    }
+}
---
+++ Week11/Day1/W11D1Assignment11.1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using W11D1Assignment11._1.Data;
+
+namespace W11D1Assignment11._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private readonly BookContext _context;
+        private Book newBook = new Book();
+
+        public MainWindow(BookContext context) {
+            InitializeComponent();
+            _context = context;
+            DataContext = newBook;
+            LoadBooks();
+        }
+
+        private void LoadBooks() {
+            BooksDataGrid.ItemsSource = _context.Books.ToList();
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e) {
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+            newBook = new Book();
+            DataContext = newBook;
+            LoadBooks();
+        }
+
+        private void DeleteBook_Click(object sender, RoutedEventArgs e) {
+            var book = (sender as FrameworkElement).DataContext as Book;
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            LoadBooks();
+        }
+    }
+}
