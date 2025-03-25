ced6a43d2e5baa24fe4d1b51202c01e0fd664125
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-03-24 20:42:14 -0700

    Week 11 Day 1 (#78)
    * Week 11 Day 1 Code 1
    
    * Week 11 Day 1 Assignment 11.1

---
+++ Week11/Day1/W11D1Assignment11.1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using W11D1Assignment11._1.Data;
+
+namespace W11D1Assignment11._1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private ServiceProvider _serviceProvider;
+
+        public App() {
+            var services = new ServiceCollection();
+            services.AddDbContext<BookContext>(options =>
+                options.UseSqlite("Data Source=Books.db"));
+            services.AddSingleton<MainWindow>();
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e) {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Week11/Day1/W11D1Assignment11.1/Data/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W11D1Assignment11._1.Data {
+    public class Book {
+        public string ISBN { get; set; } // Primary Key
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
