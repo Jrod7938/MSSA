95ec5e63f2e57781a16d77cbcba59a2bb5e261e6
Jrod7938 <Jrod7938@gmail.com>
2025-03-31 21:27:44 -0700

    Week 12 Day 1 Assignment 12.1

---
+++ Week12/Day1/W12D1Assignment12.1/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W12D1Assignment12._1 {
+    public class Node {
+        public int val { get; set; }
+        public Node? next { get; set; }
+
+        public Node(int val, Node? next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Week12/Day1/W12D1Assignment12.1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W12D1Assignment12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Part1();
+            Solution.Part2();
+        }
+    }
+}
