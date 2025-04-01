f8ca4b83d3f92946ed4df18da75349f8eaacea0b
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-03-31 20:48:06 -0700

    Week 12 Day 1 (#82)

---
+++ Week12/Day1/W12D1C1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W12D1C1
+{
+    [Serializable]
+    [Obsolete("This class will be not available in future release")]
+   
+    internal class Class1
+    {
+        [NonSerialized]
+        public string Name;
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class  DeveloperInfoAttribute:Attribute
+    {
+        private string name;
+        private string email;
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Email
+        { get { return email; } }
+        public DeveloperInfoAttribute(string name, string email)
+        {
+            this.name = name;   
+            this.email = email;
+        }
+    }
+}
---
+++ Week12/Day1/W12D1C1/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W12D1C1
+{
+    // custom attribute
+    [DeveloperInfo("Jancarlos Rodriguez","Jrod7938@gmail.com")]
+   // [DeveloperInfo("alex","dsd")]
+    internal class Demo
+    {
+    }
+}
