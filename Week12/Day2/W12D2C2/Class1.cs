51048e5f58b0bc9b0c121e7e376977a65b86a740
Jrod7938 <Jrod7938@gmail.com>
2025-04-01 12:43:34 -0700

    Week 12 Day 2 Code 2

--- Week12/Day2/W12D2C2/Class1.cs
+++ Week12/Day2/W12D2C2/Class1.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace W12D2C2
-{
-    public class Class1
-    {
+﻿namespace W12D2C2 {
+    public class Class1 {
         public int Add(int num, int num2) { return num + num2; }
         public int Multiply(int num1, int num2) { return num1 + num2; }
     }
