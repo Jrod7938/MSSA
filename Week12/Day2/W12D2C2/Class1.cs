edd22bb3e1c4e9d7f677a00e168f90cc28662fab
Jancarlos Rodriguez <46418742+Jrod7938@users.noreply.github.com>
2025-04-02 09:10:06 -0700

    Week 12 Day 2 (#84)
    * Week 12 Day 2 Code 1
    
    * Week 12 Day 2 Code 2
    
    * Week 12 Day 2 Assignment 12.2

---
+++ Week12/Day2/W12D2C2/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace W12D2C2 {
+    public class Class1 {
+        public int Add(int num, int num2) { return num + num2; }
+        public int Multiply(int num1, int num2) { return num1 + num2; }
+    }
+}
